3752bf92920c51dec1da8b8ce7f1b8b1eea2a871
Anil Madhavapeddy <anil@recoil.org>
2013-09-04 11:07:32 +0100

    remove trailing whitespace

--- bin/opam_changelog.ml
+++ bin/opam_changelog.ml
@@ -9,23 +9,23 @@ let changelog branch user repo =
   ) clog;
   `Ok ()
 
-let branch = 
+let branch =
   let doc = "Remote branch to retrieve CHANGES from" in
   Arg.(value & opt string "master" & info ["b"; "branch"] ~docv:"BRANCH" ~doc)
 
-let user = 
+let user =
   let doc = "Github username to query (will form URL of form https://github.com/$(i,USER)" in
   Arg.(required & pos 0 (some string) None & info [] ~doc ~docv:"USER")
 
-let repo = 
+let repo =
   let doc = "Github repository to query" in
   Arg.(required & pos 1 (some string) None & info [] ~doc ~docv:"REPO")
 
-let cmd : (unit Term.t * Term.info) = 
+let cmd : (unit Term.t * Term.info) =
   let doc = "Query changelog from remote repo and make sure it is well-formed and parseable" in
   let man = [
-    `S "BUGS"; 
-    `P "Email them to <anil@recoil.org>."; 
+    `S "BUGS";
+    `P "Email them to <anil@recoil.org>.";
     `S "SEE ALSO";
     `P "$(b,opam)(1)" ]
   in
